0803645e3504b64d5755c497bcb116387d9fddbe
User <vakho.daraselia@gmail.com>
2024-06-21 16:25:57 +0400

    fix about section progress bar

--- app/[locale]/components/AboutSection/AboutSectionDescriptions.tsx
+++ app/[locale]/components/AboutSection/AboutSectionDescriptions.tsx
@@ -1,10 +1,12 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 
 function AboutSectionDescriptions() {
   const { t } = useTranslation("common");
+  const [activeIndex, setActiveIndex] = useState(0);
+  const [progress, setProgress] = useState(0);
 
   const items = [
     {
@@ -21,19 +23,28 @@ function AboutSectionDescriptions() {
     },
   ];
 
-  const [activeIndex, setActiveIndex] = useState(0);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setProgress((prev) => {
+        setActiveIndex(
+          prev >= 0 && prev < 34 ? 0 : prev >= 34 && prev < 67 ? 1 : 2
+        );
+        return prev === 100 ? 0 : prev + 1;
+      });
+    }, 100);
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+  // console.log(progress);
 
   return (
     <div className="flex">
       <div className="relative w-[16px] flex flex-col bg-[#D9D9D9] rounded-lg">
-        {items.map((_, i) => (
-          <div
-            key={i}
-            className={`transition-all duration-300 rounded-lg flex-1 ${
-              activeIndex === i ? "bg-primary" : ""
-            }`}
-          />
-        ))}
+        <div
+          className={`transition-all duration-300 rounded-lg bg-primary`}
+          style={{ height: `${progress}%` }}
+        />
       </div>
       <div className="flex flex-col gap-5">
         {items.map((item, i) => (
@@ -42,11 +53,8 @@ function AboutSectionDescriptions() {
             className={`flex flex-col gap-5 p-5 transition-all cursor-pointer ${
               activeIndex !== i ? "text-[#656565]" : ""
             }`}
-            onClick={() => {
-              setActiveIndex(i);
-            }}
           >
-            <h1 className="font-[500] text-[24px]">{item.title}</h1>
+            <h1 className="font-bold text-[24px]">{item.title}</h1>
             <p>{item.description}</p>
           </div>
         ))}
--- app/[locale]/components/Hero/HomeHero.tsx
+++ app/[locale]/components/Hero/HomeHero.tsx
@@ -1,25 +1,21 @@
 "use client";
-import { CustomVerticalProgress } from "@/components";
-import React, { useEffect, useState } from "react";
 
 export function HomeHero() {
-  const [progress, setProgress] = useState(0);
+  // const [progress, setProgress] = useState(0);
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setProgress((prev) => (prev === 100 ? 0 : prev + 1));
-    }, 100);
-    return () => {
-      clearInterval(interval);
-    };
-  }, []);
+  // useEffect(() => {
+  //   const interval = setInterval(() => {
+  //     setProgress((prev) => (prev === 100 ? 0 : prev + 1));
+  //   }, 100);
+  //   return () => {
+  //     clearInterval(interval);
+  //   };
+  // }, []);
   return (
     <div
       className="bg-hero-mobile md:bg-hero-desktop bg-cover bg-center  h-[100vh]  "
       id="home"
     >
-      <CustomVerticalProgress progress={progress} />
-
       {/* <ContainerDiv>
         <h1>asdfasdf</h1>
         <p>asdfasdf</p>
