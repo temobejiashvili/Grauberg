d89fe73b32e172be40951ea3144ff6bc5c8d3e8d
David3855 <152386547+David3855@users.noreply.github.com>
2025-01-09 10:28:59 +0400

    fix: lang

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,4 +1,5 @@
 import React, { ReactNode } from "react";
+import { dir } from "i18next";
 import i18nConfig from "@/i18nConfig";
 import type { Metadata } from "next";
 import FooterComponent from "@/components/Footer/FooterComponent";
@@ -37,7 +38,7 @@ export default async function RootLayout({
   const { t, resources } = await initializeTranslations(locale, i18nNamespaces);
 
   return (
-    <html lang="ge" className={notoGeorgian.className}>
+    <html lang={locale} dir={dir(locale)} className={notoGeorgian.className}>
       <head>
         <link rel="icon" href="/images/Vector.png" />
       </head>
