9645fa3bb541358efe9f7dbdc6a9271b380be367
GiorgiKintsu <giorgi.kintsu@gmail.com>
2025-01-17 13:04:59 +0400

    feat: add translates && fix contact and catalog responsives

--- components/blogCard/BlogCard.tsx
+++ components/blogCard/BlogCard.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
+import { TranslateText } from "../translateText/TranslateText";
 
 interface BlogCardProps {
   image?: string;
   alt?: string;
-  title?: string;
-  text?: string;
+  title: string;
+  text: string;
   url: string;
 }
 
@@ -37,11 +38,11 @@ const BlogCard: React.FC<BlogCardProps> = ({
         />
       </aside>
       <aside className="py-[20px] px-[17px] border-[1px] border-solid border-[#D6D6D6] rounded-bl-[16px] rounded-br-[16px]">
-        <h3 className="text-[24px] text-[#100F0F] font-bold leading-[32px] max-900:text-[20px] max-900:leading-[30px]">
-          {title}
+        <h3 className="h-[64px] text-[24px] text-[#100F0F] font-bold leading-[32px] max-900:text-[20px] max-900:leading-[30px] overflow-hidden text-ellipsis line-clamp-2">
+          <TranslateText text={title} />
         </h3>
-        <p className="text-[14px] text-[#5C5C5C] leading-[20px] mt-[11px] ">
-          {text}
+        <p className="h-[64px] text-[14px] text-[#5C5C5C] leading-[20px] mt-[11px] overflow-hidden text-ellipsis line-clamp-3">
+          <TranslateText text={text} />
         </p>
       </aside>
     </Link>
